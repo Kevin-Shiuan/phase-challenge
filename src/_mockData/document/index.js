5524d790149299b417d37eb78bc77b95837f216f
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-05-25 21:43:56 +0800

    [feat] create data structure & mock data

---
+++ src/_mockData/document/index.js
@@ -0,0 +1,18 @@
+import page1 from './page1';
+import page2 from './page2';
+import frame1 from './frame1';
+import frame2 from './frame2';
+import frame3 from './frame3';
+import structure from './structure';
+
+export const documentData = {
+  page1,
+  page2,
+  frame1,
+  frame2,
+  frame3,
+};
+
+export const documentStructure = structure;
+
+export const documentPages = [page1, page2];
