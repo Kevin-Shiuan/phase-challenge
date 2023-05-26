b676aee158be9d4bb0890680367e6b0360e6eea5
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-05-26 14:03:52 +0800

    [refractor] change redux to recoil

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import App from "./App";
-import "./styles.css";
+import App from './App';
+import './styles.css';
 
-// redux
-import store from './redux/Store'
-import { Provider } from 'react-redux'
+// recoil
+import { RecoilRoot } from 'recoil';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   rootElement
 );
