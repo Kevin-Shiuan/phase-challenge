69e8d70c9b60b5e9e53be0db8edca5f759f93bb9
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-05-28 01:10:19 +0800

    [chore] modify mock data

--- src/_mockData/document/index.js
+++ src/_mockData/document/index.js
@@ -3,17 +3,9 @@ import page2 from './page2';
 import frame1 from './frame1';
 import frame2 from './frame2';
 import frame3 from './frame3';
-import structure from './structure';
+import document from './document';
 
-// export const documentData = {
-//   page1,
-//   page2,
-//   frame1,
-//   frame2,
-//   frame3,
-// };
-
-export const documentStructure = structure;
+export default document;
 
 export const documentPages = [page1, page2];
 
