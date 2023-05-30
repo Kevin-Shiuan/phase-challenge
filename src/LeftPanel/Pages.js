b34df30c4927e15bc15b226559d1777234b40824
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-05-30 20:33:12 +0800

    [feat] page renaming

--- src/LeftPanel/Pages.js
+++ src/LeftPanel/Pages.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { useSetRecoilState, useRecoilValue, useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { documentState, pageState, pageSelector } from '../recoil';
 
@@ -7,6 +6,7 @@ const PagesWrapper = styled.div`
   border-bottom: 1px solid;
   padding-bottom: 16px;
 `;
+
 const Pages = () => {
   const document = useRecoilValue(documentState);
 
@@ -21,15 +21,45 @@ const Pages = () => {
 };
 
 const Page = ({ id }) => {
-  // recoil
-  // get the state of the desire frame
-  const page = useRecoilValue(pageState(id));
+  // get the state of the desire page from recoil
+  const [page, setPage] = useRecoilState(pageState(id));
   // update function to update the selected frame
   const updateSlectedFrame = useSetRecoilState(pageSelector(id));
 
+  const handleRenaming = (e) => {
+    setPage((page) => ({ ...page, renaming: true }));
+  };
+  const handleDoneRename = (newName) => {
+    if (newName === '') return cancelRename();
+    setPage((page) => ({ ...page, name: newName, renaming: false }));
+  };
+  const cancelRename = () => {
+    setPage((page) => ({ ...page, renaming: false }));
+  };
+
   return (
     <div key={page.id} id={page.id} onClick={() => updateSlectedFrame()} style={{ padding: '0.5rem' }}>
-      {page.selected ? <strong>{page.name}</strong> : page.name}
+      {page.selected ? (
+        <div onDoubleClick={() => handleRenaming()}>
+          {page.renaming ? (
+            <input
+              type="text"
+              className="bg-transparent rounded-sm outline outline-2 outline-offset-4"
+              defaultValue={page.name}
+              autoFocus
+              onKeyDown={(e) => {
+                if (e.key === 'Escape') cancelRename();
+                if (e.key === 'Enter') handleDoneRename(e.currentTarget.value);
+              }}
+              onBlur={(e) => handleDoneRename(e.currentTarget.value)}
+            />
+          ) : (
+            <strong>{page.name}</strong>
+          )}
+        </div>
+      ) : (
+        page.name
+      )}
     </div>
   );
 };
