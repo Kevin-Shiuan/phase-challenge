11be7cc9e0a729500e1c60bd1b5bc28752713b4a
Kevin-Shiuan <b07901120@g.ntu.edu.tw>
2023-06-01 17:48:21 +0800

    [feat] add more mock data

--- src/_mockData/document/index.js
+++ src/_mockData/document/index.js
@@ -3,6 +3,9 @@ import frame1 from './frame1';
 import frame2 from './frame2';
 import frame3 from './frame3';
 import frame4 from './frame4';
+import frame5 from './frame5';
+import frame6 from './frame6';
+import frame7 from './frame7';
 import page1 from './page1';
 import page2 from './page2';
 
@@ -10,4 +13,4 @@ export default document;
 
 export const documentPages = [page1, page2];
 
-export const documentFrames = [frame1, frame2, frame3, frame4];
+export const documentFrames = [frame1, frame2, frame3, frame4, frame5, frame6, frame7];
